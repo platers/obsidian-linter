d8a99a9ce8db63e4db12af8fed61de7e10ea89b6
platers <platers81@gmail.com>
2021-08-02 23:07:11 -0400

    spaces after list markers

--- rules.ts
+++ rules.ts
@@ -6,4 +6,7 @@ export const rules: { [name: string] : (text: string) => string } =
 	"newlines_around_headings" : (text: string) => {
 		return text.replace(/\n*^(#+ .*)\n*/gm, "\n\n$1\n\n");
 	},
+	"spaces_after_list_markers" : (text: string) => {
+		return text.replace(/^(\s*\d+\.|[-+*])\s+/gm, "$1 ");
+	}
 };
