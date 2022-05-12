cc81366a4f15c030e19541f7812ae712d50f995d
Michael Naumov <mnaoumov@gmail.com>
2022-05-11 19:14:22 -0600

    Add fix to long titles (#198)
    * Escape title to be valid YAML
    
    * Fix eslint build
    
    * Add tests
    
    * Prevent title line breaks
    
    * Add test
    
    * Fix eslint build

--- src/test.ts
+++ src/test.ts
@@ -658,4 +658,19 @@ describe('YAML Title', () => {
 
     expect(rulesDict['yaml-title'].apply(before, {'Title Key': 'title'})).toBe(after);
   });
+
+  it('Does not insert line breaks for long title', () => {
+    const before = dedent`
+    # Very long title 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+    `;
+
+    const after = dedent`
+    ---
+    title: Very long title 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+    ---
+    # Very long title 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+    `;
+
+    expect(rulesDict['yaml-title'].apply(before, {'Title Key': 'title'})).toBe(after);
+  });
 });
