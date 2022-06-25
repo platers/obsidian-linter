ba242ce470af2129c58914c88059429bdbb45e9e
Peter Kaufman <PeterJamesKaufman@gmail.com>
2022-06-25 15:11:04 -0400

    updated link regex to remove the possibility of nesting braces and new line characters while adding a test to verify that the issue is fixed and that we do not accidentally recreate it later

--- src/test.ts
+++ src/test.ts
@@ -976,6 +976,20 @@ describe('Links', () => {
 
     expect(rulesDict['capitalize-headings'].apply(before, {'Style': 'First letter'})).toBe(after);
   });
+  // accounts for https://github.com/platers/obsidian-linter/issues/236
+  it('Link after checkbox is not affected by removing whitespace in links', () => {
+    const before = dedent`
+    - [ ] [Link text](path/fileName.md)
+    - [ ] [[fileName]]
+    `;
+
+    const after = dedent`
+    - [ ] [Link text](path/fileName.md)
+    - [ ] [[fileName]]
+    `;
+
+    expect(rulesDict['remove-link-spacing'].apply(before)).toBe(after);
+  });
 });
 
 describe('Remove Multiple Spaces', () => {
