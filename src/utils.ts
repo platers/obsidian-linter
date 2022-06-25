212b9ebe582299a9a39cd3cd28982c1d8b786a57
Peter Kaufman <PeterJamesKaufman@gmail.com>
2022-06-25 15:39:45 -0400

    had to ignore YAML for the addition of 2 spaces between continued content since for some reason remark considers the list form of an array to make the YAML sections no longer exempt from certain checks

--- src/utils.ts
+++ src/utils.ts
@@ -16,6 +16,10 @@ export const codeBlockRegex = new RegExp(`${backtickBlockRegexTemplate}|${tildeB
 export const linkRegex = /((!?)\[[^[\n]*\]\([^[\n]*\))|(\[{2}[^[\n]*\]{2})/g;
 export const tagRegex = /#[^\s#]{1,}/g;
 
+// Reused placeholders
+
+const yamlPlaceholder = '---\n---';
+
 // Helper functions
 
 /**
@@ -100,6 +104,12 @@ export function makeEmphasisOrBoldConsistent(text: string, style: string, type: 
  * @return {string} The text with two spaces at the end of lines of paragraphs, list items, and blockquotes where there were consecutive lines of content.
  */
 export function addTwoSpacesAtEndOfLinesFollowedByAnotherLineOfTextContent(text: string): string {
+  // added the placeholder for YAML in order to prevent the accidental misconstruing of the YAML as a paragraph when an array is in multiline form (i.e. it has a list for the array elements)
+  const yamlMatches = text.match(yamlRegex);
+  if (yamlMatches) {
+    text = text.replace(yamlMatches[0], escapeDollarSigns(yamlPlaceholder));
+  }
+
   const positions: Position[] = getPositions('paragraph', text);
   if (positions.length === 0) {
     return text;
@@ -124,6 +134,10 @@ export function addTwoSpacesAtEndOfLinesFollowedByAnotherLineOfTextContent(text:
     }
 
     text = text.substring(0, position.start.offset) + paragraphLines.join('\n') + text.substring(position.end.offset);
+  }
+
+  if (yamlMatches) {
+    text = text.replace(yamlPlaceholder, escapeDollarSigns(yamlMatches[0]));
   }
 
   return text;
@@ -180,7 +194,6 @@ export function ignoreCodeBlocksYAMLTagsAndLinks(text: string, func: (text: stri
   text = ret.text;
   const replacedCodeBlocks = ret.replacedCodeBlocks;
 
-  const yamlPlaceholder = '---\n---';
   const yamlMatches = text.match(yamlRegex);
   if (yamlMatches) {
     text = text.replace(yamlMatches[0], escapeDollarSigns(yamlPlaceholder));
