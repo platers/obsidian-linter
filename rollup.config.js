776d86002eafa50d73d748c7a7e9d43a762ddfee
Michael Naumov <mnaoumov@gmail.com>
2022-07-16 18:29:09 -0600

    Exclude obsidian dependency from docs generator

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import externalGlobals from 'rollup-plugin-external-globals';
 
 const isProd = (process.env.BUILD === 'production');
 
@@ -42,6 +43,9 @@ export default [
       typescript(),
       nodeResolve({browser: true}),
       commonjs(),
+      externalGlobals({
+        'obsidian': 'obsidian'
+      })
     ]
   }
 ];
