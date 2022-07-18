ec99a920aeb727b7f3cee1004122221b45dae15b
Michael Naumov <mnaoumov@gmail.com>
2022-07-18 00:37:41 -0600

    Add generic builders

---
+++ src/rules/rule-builder.ts
@@ -0,0 +1,89 @@
+import {Example, Options, Rule, RuleType} from '../rules';
+import {BooleanOption, Option} from '../option';
+
+export abstract class RuleBuilderBase {
+  static #ruleMap = new Map<string, Rule>();
+
+  static getRule<TOptions extends Options>(this: (new() => RuleBuilder<TOptions>)): Rule {
+    if (!RuleBuilderBase.#ruleMap.has(this.name)) {
+      const builder = new this();
+      const rule = new Rule(builder.name, builder.description, builder.type, builder.safeApply.bind(builder), builder.exampleBuilders.map((b) => b.example), builder.optionBuilders.map((b) => b.option));
+      RuleBuilderBase.#ruleMap.set(this.name, rule);
+    }
+
+    return RuleBuilderBase.#ruleMap.get(this.name);
+  }
+}
+
+export default abstract class RuleBuilder<TOptions extends Options> extends RuleBuilderBase {
+  abstract get OptionsClass(): (new() => TOptions);
+
+  safeApply(text: string, options?: Options): string {
+    options = options ?? {};
+    const defaultOptions = new this.OptionsClass();
+    const ruleOptions = Object.assign(defaultOptions, options) as TOptions;
+
+    for (const optionBuilder of this.optionBuilders) {
+      const optionsKey = optionBuilder.optionsKey as keyof TOptions;
+      if (ruleOptions[optionsKey] === undefined) {
+        ruleOptions[optionsKey] = options[optionBuilder.name];
+      }
+    }
+    return this.apply(text, ruleOptions);
+  }
+
+  abstract get name(): string;
+  abstract get description(): string;
+  abstract get type(): RuleType;
+  abstract apply(text: string, options: TOptions): string;
+  abstract get exampleBuilders(): ExampleBuilder<TOptions>[];
+  abstract get optionBuilders(): OptionBuilder<TOptions, any>[];
+}
+
+export class ExampleBuilder<TOptions extends Options> {
+  readonly example: Example;
+
+  constructor(args: {
+    description: string,
+    before: string,
+    after: string,
+    options?: TOptions
+  }) {
+    this.example = new Example(args.description, args.before, args.after, args.options);
+  }
+}
+
+type KeysMatchingValueType<TObject, TValue> = {[TKey in keyof TObject]-?: TObject[TKey] extends TValue ? TKey : never}[keyof TObject];
+
+export abstract class OptionBuilder<TOptions extends Options, TValue> {
+  readonly OptionsClass: (new() => TOptions);
+  readonly name: string;
+  readonly description: string;
+  readonly optionsKey: string & KeysMatchingValueType<TOptions, TValue>;
+  readonly option: Option;
+
+  constructor(args: {
+    OptionsClass: (new() => TOptions),
+    name: string
+    description: string,
+    optionsKey: string & KeysMatchingValueType<TOptions, TValue>;
+  }) {
+    this.OptionsClass = args.OptionsClass;
+    this.name = args.name;
+    this.description = args.description;
+    this.optionsKey = args.optionsKey;
+    this.option = this.buildOption();
+  }
+
+  protected get defaultValue(): TValue {
+    return new this.OptionsClass()[this.optionsKey];
+  }
+
+  protected abstract buildOption(): Option;
+}
+
+export class BooleanOptionBuilder<TOptions extends Options> extends OptionBuilder<TOptions, boolean> {
+  protected buildOption(): Option {
+    return new BooleanOption(this.name, this.description, this.defaultValue);
+  }
+}
