70dca3ee4dedb81567b5e6010c70098d8a9995c1
Michael Naumov <mnaoumov@gmail.com>
2022-07-20 13:45:00 -0600

    Enable decorators in babel
    Needed for jest tests

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,8 @@ module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
+  ],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { version: 'legacy' }]
   ]
 };
