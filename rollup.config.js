9cc9e8bc177b8c214810fc070a25dbdb8a2676ab
Bridget Loughhead <bridget.loughhead@servian.com>
2022-07-20 14:33:51 +1000

    Add rollup-plugin-terser and terser plugin to minify build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
 const isProd = (process.env.BUILD === 'production');
 
@@ -21,12 +22,16 @@ export default [
       format: 'cjs',
       exports: 'default',
       banner,
+      plugins: [
+        terser(),
+      ]
     },
     external: ['obsidian'],
     plugins: [
       typescript(),
       nodeResolve({browser: true}),
       commonjs(),
+      terser(),
     ]
   },
   {
