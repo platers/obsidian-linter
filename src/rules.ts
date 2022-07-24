7032ad979ad7e57adbdbffcfa9c260c18ba3768e
Michael Naumov <mnaoumov@gmail.com>
2022-07-23 23:56:30 -0600

    Extract FileNameHeading

--- src/rules.ts
+++ src/rules.ts
@@ -1,8 +1,5 @@
 import dedent from 'ts-dedent';
 import {
-  insert,
-} from './utils/strings';
-import {
   loadYAML,
 } from './utils/yaml';
 import {
@@ -174,57 +171,6 @@ export function getDisabledRules(text: string): string[] {
 }
 
 export const rules: Rule[] = [
-  new Rule(
-      'File Name Heading',
-      'Inserts the file name as a H1 heading if no H1 heading exists.',
-      RuleType.HEADING,
-      (text: string, options = {}) => {
-        return ignoreListOfTypes([IgnoreTypes.code, IgnoreTypes.yaml, IgnoreTypes.link, IgnoreTypes.wikiLink, IgnoreTypes.tag], text, (text) => {
-        // check if there is a H1 heading
-          const hasH1 = text.match(/^#\s.*/m);
-          if (hasH1) {
-            return text;
-          }
-
-          const fileName = options['metadata: file name'];
-          // insert H1 heading after front matter
-          let yaml_end = text.indexOf('\n---');
-          yaml_end =
-          yaml_end == -1 || !text.startsWith('---\n') ? 0 : yaml_end + 5;
-          return insert(text, yaml_end, `# ${fileName}\n`);
-        });
-      },
-      [
-        new Example(
-            'Inserts an H1 heading',
-            dedent`
-              This is a line of text
-            `,
-            dedent`
-              # File Name
-              This is a line of text
-            `,
-            {'metadata: file name': 'File Name'},
-        ),
-        new Example(
-            'Inserts heading after YAML front matter',
-            dedent`
-              ---
-              title: My Title
-              ---
-              This is a line of text
-            `,
-            dedent`
-              ---
-              title: My Title
-              ---
-              # File Name
-              This is a line of text
-            `,
-            {'metadata: file name': 'File Name'},
-        ),
-      ],
-  ),
   new Rule(
       'Capitalize Headings',
       'Headings should be formatted with capitalization',
---
+++ src/rules/file-name-heading.ts
@@ -0,0 +1,80 @@
+import {Options, RuleType} from '../rules';
+import RuleBuilder, {ExampleBuilder, OptionBuilderBase} from './rule-builder';
+import dedent from 'ts-dedent';
+import {ignoreListOfTypes, IgnoreTypes} from '../utils/ignore-types';
+import {insert} from '../utils/strings';
+
+class FileNameHeadingOptions implements Options {
+  fileName: string
+}
+
+@RuleBuilder.register
+export default class FileNameHeading extends RuleBuilder<FileNameHeadingOptions> {
+  get OptionsClass(): new () => FileNameHeadingOptions {
+    return FileNameHeadingOptions;
+  }
+  get name(): string {
+    return 'File Name Heading';
+  }
+  get description(): string {
+    return 'Inserts the file name as a H1 heading if no H1 heading exists.';
+  }
+  get type(): RuleType {
+    return RuleType.HEADING;
+  }
+  apply(text: string, options: FileNameHeadingOptions): string {
+    return ignoreListOfTypes([IgnoreTypes.code, IgnoreTypes.yaml, IgnoreTypes.link, IgnoreTypes.wikiLink, IgnoreTypes.tag], text, (text) => {
+      // check if there is a H1 heading
+      const hasH1 = text.match(/^#\s.*/m);
+      if (hasH1) {
+        return text;
+      }
+
+      const fileName = options.fileName;
+      // insert H1 heading after front matter
+      let yaml_end = text.indexOf('\n---');
+      yaml_end =
+        yaml_end == -1 || !text.startsWith('---\n') ? 0 : yaml_end + 5;
+      return insert(text, yaml_end, `# ${fileName}\n`);
+    });
+  }
+  get exampleBuilders(): ExampleBuilder<FileNameHeadingOptions>[] {
+    return [
+      new ExampleBuilder({
+        description: 'Inserts an H1 heading',
+        before: dedent`
+          This is a line of text
+        `,
+        after: dedent`
+          # File Name
+          This is a line of text
+        `,
+        options: {
+          fileName: 'File Name',
+        },
+      }),
+      new ExampleBuilder({
+        description: 'Inserts heading after YAML front matter',
+        before: dedent`
+          ---
+          title: My Title
+          ---
+          This is a line of text
+        `,
+        after: dedent`
+          ---
+          title: My Title
+          ---
+          # File Name
+          This is a line of text
+        `,
+        options: {
+          fileName: 'File Name',
+        },
+      }),
+    ];
+  }
+  get optionBuilders(): OptionBuilderBase<FileNameHeadingOptions>[] {
+    return [];
+  }
+}
