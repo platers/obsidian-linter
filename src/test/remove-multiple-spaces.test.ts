83591616fe2f2e5c8d74f4a586ac12f94aad0a66
Michael Naumov <mnaoumov@gmail.com>
2022-07-24 13:18:14 -0600

    Recover missing remove-multiple-spaces tests

--- src/test/remove-multiple-spaces.test.ts
+++ src/test/remove-multiple-spaces.test.ts
@@ -21,6 +21,19 @@ ruleTest({
       `,
     },
     {
+      testName: 'Make sure spaces at the start of the line are ignored',
+      before: dedent`
+          Paragraph contents are here
+          Second paragraph here...
+        
+      `,
+      after: dedent`
+          Paragraph contents are here
+          Second paragraph here...
+        
+      `,
+    },
+    {
       testName: 'Make sure non-letter followed or preceeded by 2 spaces has them cut down to 1 space',
       before: dedent`
         # Hello world
@@ -45,7 +58,6 @@ ruleTest({
     {
       // accounts for https://github.com/platers/obsidian-linter/issues/244
       testName: 'Tables are ignored',
-
       before: dedent`
         # Table 1
 
@@ -86,15 +98,85 @@ ruleTest({
       `,
     },
     {
+      // accounts for https://github.com/platers/obsidian-linter/issues/289
+      testName: 'Callouts and block quotes allow multiple spaces at start to allow for code and list item indentation',
+      before: dedent`
+        # List Item in Callout
+
+        > [!info] Unordered List
+        > - First Level
+        > - First Level
+        >   - Second Level
+        >     - Third Level
+        > - First Level
+
+        # Code in Callout
+
+        > [!info] Code
+        >     Line 1
+        >     Line 2
+        >     Line 3
+
+        # List Item in Block Quote
+
+        > Unordered List
+        > - First Level
+        > - First Level
+        >   - Second Level
+        >     - Third Level
+        > - First Level
+
+        # Code in Block Quote
+
+        > Code
+        >     Line 1
+        >     Line 2
+        >     Line 3
+      `,
+      after: dedent`
+        # List Item in Callout
+
+        > [!info] Unordered List
+        > - First Level
+        > - First Level
+        >   - Second Level
+        >     - Third Level
+        > - First Level
+
+        # Code in Callout
+
+        > [!info] Code
+        >     Line 1
+        >     Line 2
+        >     Line 3
+
+        # List Item in Block Quote
+
+        > Unordered List
+        > - First Level
+        > - First Level
+        >   - Second Level
+        >     - Third Level
+        > - First Level
+
+        # Code in Block Quote
+
+        > Code
+        >     Line 1
+        >     Line 2
+        >     Line 3
+      `,
+    },
+    {
       testName: 'Multiple spaces after ">" are still removed if not the start of a line',
       before: dedent`
         # Text with > with multiple spaces after it
-          
+
         Text >  other text
       `,
       after: dedent`
         # Text with > with multiple spaces after it
-          
+
         Text > other text
       `,
     },
