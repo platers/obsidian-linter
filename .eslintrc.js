585d03596f4e20f683d402c2e9d192e1dd46157c
Michael Naumov <mnaoumov@gmail.com>
2022-07-25 08:17:17 -0600

    Migrade eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "jest/globals": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "google"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint",
+    "jest"
+  ],
+  "rules": {
+    "camelcase": "off",
+    "max-len": "off",
+    "require-jsdoc": "off"
+  }
+};
