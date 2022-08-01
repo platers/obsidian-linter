32ec0763410d41728cdd6df59bac0b23e99c35e6
Peter Kaufman <PeterJamesKaufman@gmail.com>
2022-07-31 23:16:35 -0400

    ignore common.ts in __test__ so that it does not fail when trying to run tests due to that file not having any tests in it

--- jest.config.ts
+++ jest.config.ts
@@ -8,5 +8,10 @@ const config: Config.InitialOptions = {
   transformIgnorePatterns: [
     '"/node_modules/(?!unified-lint-rule)',
   ],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+    '!**/__tests__/common.ts',
+  ],
 };
 export default config;
