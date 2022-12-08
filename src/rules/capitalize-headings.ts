2d70fd4428985118b909428ac0accc9265fc829c
Peter Kaufman <PeterJamesKaufman@gmail.com>
2022-12-07 21:28:33 -0500

    simplified a little bit of logic

--- src/rules/capitalize-headings.ts
+++ src/rules/capitalize-headings.ts
@@ -104,9 +104,7 @@ export default class CapitalizeHeadings extends RuleBuilder<CapitalizeHeadingsOp
           }
         }
 
-        headerText = headerWords.join(' ');
-
-        return headerText;
+        return headerWords.join(' ');
       });
     });
   }
