d1eab99496e84e58e64c727f463c80314abdb4f7
Peter Kaufman <PeterJamesKaufman@gmail.com>
2023-08-19 06:41:28 -0400

    added logic around copying and pasting from text areas in settings for debugging settings rather than needing to manually copy and paste to help make things easier to get to

--- src/ui/components/text-box-full.ts
+++ src/ui/components/text-box-full.ts
@@ -1,7 +1,12 @@
+import {setIcon} from 'obsidian';
+
 export class TextBoxFull {
   nameEl: HTMLDivElement;
   descEl: HTMLDivElement;
+  inputContainerEl: HTMLDivElement;
   inputEl: HTMLTextAreaElement;
+  copyEl: HTMLDivElement;
+  copyIconEl: HTMLSpanElement;
   constructor(public containerEl: HTMLElement, private name: string, private description: string, private disabled: boolean = true) {
     this.display();
   }
@@ -16,12 +21,37 @@ export class TextBoxFull {
     this.descEl = infoEl.createDiv('setting-item-description');
     this.descEl.setText(this.description);
 
-    this.inputEl = settingEl.createDiv().createEl('textarea', {cls: 'full-width'});
+    this.inputContainerEl = settingEl.createDiv('full-width-textbox-input-wrapper');
+    this.inputContainerEl.onmouseover = () => {
+      this.copyEl.removeClass('linter-visually-hidden');
+    };
+    this.inputContainerEl.onmouseleave = () => {
+      this.copyEl.addClass('linter-visually-hidden');
+    };
+    this.inputEl = this.inputContainerEl.createEl('textarea', {cls: 'full-width'});
     this.inputEl.spellcheck = false;
     this.inputEl.disabled = this.disabled;
+
+    this.copyEl = this.inputContainerEl.createDiv({'cls': 'settings-copy-button linter-visually-hidden ', 'attr': {'aria-label': 'Copy'}});
+    this.copyIconEl = this.copyEl.createSpan();
+    setIcon(this.copyIconEl, 'linter-clipboard');
+    this.copyIconEl.onclick = () => {
+      this.handleCopy(this.copyIconEl);
+    };
   }
 
   getInput(): string {
     return this.inputEl.value;
+  }
+
+  handleCopy(copyEl: HTMLSpanElement) {
+    navigator.clipboard.writeText(this.getInput()).then(() => {
+      setIcon(copyEl, 'linter-success');
+      setTimeout(() => {
+        setIcon(copyEl, 'linter-clipboard');
+      }, 1500);
+    }, () => {
+      this.copyEl.innerText = 'Error';
+    });
   }
 }
