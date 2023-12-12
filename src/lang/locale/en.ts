4d676220baa530b3178bebac0ecf29698f0a25fa
Peter Kaufman <PeterJamesKaufman@gmail.com>
2023-12-12 07:55:25 -0500

    make wording more precise for move footnotes

--- src/lang/locale/en.ts
+++ src/lang/locale/en.ts
@@ -446,7 +446,7 @@ export default {
     // move-footnotes-to-the-bottom.ts
     'move-footnotes-to-the-bottom': {
       'name': 'Move Footnotes to the bottom',
-      'description': 'Move all footnotes to the bottom of the document and makes sure they are sorted based on reference order.',
+      'description': 'Move all footnotes to the bottom of the document and makes sure they are sorted based on the order they are referenced in the file\'s body.',
     },
     // move-math-block-indicators-to-their-own-line.ts
     'move-math-block-indicators-to-their-own-line': {
