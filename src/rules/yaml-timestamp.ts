55ad6096510a5e00018adfa8b038871c50534d41
Peter Kaufman <PeterJamesKaufman@gmail.com>
2024-04-29 19:58:17 -0400

    swapped to only update a retained value if the timestamp format has changed since it causes issues with timezone created dates otherwise

--- src/rules/yaml-timestamp.ts
+++ src/rules/yaml-timestamp.ts
@@ -102,7 +102,10 @@ export default class YamlTimestamp extends RuleBuilder<YamlTimestampOptions> {
       textModified = true;
     } else if (keyWithValueFound) {
       const createdDateString = this.getYAMLTimestampString(text, created_match, options.dateCreatedKey);
-      if (options.forceRetentionOfCreatedValue) {
+
+      // @ts-expect-error
+      const actualFormat = parseFormat(createdDateString);
+      if (options.forceRetentionOfCreatedValue && options.format !== actualFormat) {
         const yamlCreatedDateTime = this.parseValueToCurrentFormatIfPossible(createdDateString, options.format, options.locale);
         if (yamlCreatedDateTime == null) {
           throw new Error(getTextInLanguage('logs.invalid-date-format-error').replace('{DATE}', createdDateString).replace('{FILE_NAME}', options.fileName));
@@ -117,7 +120,7 @@ export default class YamlTimestamp extends RuleBuilder<YamlTimestampOptions> {
 
           textModified = true;
         }
-      } else {
+      } else if (!options.forceRetentionOfCreatedValue) {
         const createdDateTime = moment(createdDateString, options.format, options.locale, true);
         if (createdDateTime == undefined || !createdDateTime.isValid()) {
           text = text.replace(
@@ -178,7 +181,7 @@ export default class YamlTimestamp extends RuleBuilder<YamlTimestampOptions> {
       return desiredFormatDate;
     }
 
-    // @ts-ignore
+    // @ts-expect-error
     const actualFormat = parseFormat(timestamp);
     if (actualFormat != undefined) {
       const date = moment(timestamp, actualFormat);
