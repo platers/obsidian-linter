89e9d7ffdc0a26cb4b9ed6f619b26400d2cd0e72
Trent Oliver <9102481+tcoliver@users.noreply.github.com>
2024-07-25 21:45:35 -0400

    moved yaml-title to runAfterRegularRules
    yaml-title needs to run after capitalize-headings since it may be
    configured to use the first H1 heading as the title value.

--- src/rules-runner.ts
+++ src/rules-runner.ts
@@ -21,6 +21,7 @@ import {LintCommand} from './ui/linter-components/custom-command-option';
 import {convertStringVersionOfEscapeCharactersToEscapeCharacters} from './utils/strings';
 import {getTextInLanguage} from './lang/helpers';
 import CapitalizeHeadings from './rules/capitalize-headings';
+import YamlTitle from './rules/yaml-title';
 import BlockquoteStyle from './rules/blockquote-style';
 import {IgnoreTypes, ignoreListOfTypes} from './utils/ignore-types';
 import MoveMathBlockIndicatorsToOwnLine from './rules/move-math-block-indicators-to-own-line';
@@ -115,6 +116,11 @@ export class RulesRunner {
     const postRuleLogText = getTextInLanguage('logs.post-rules');
     timingBegin(postRuleLogText);
     [newText] = CapitalizeHeadings.applyIfEnabled(newText, runOptions.settings, this.disabledRules);
+
+    [newText] = YamlTitle.applyIfEnabled(newText, runOptions.settings, this.disabledRules, {
+      fileName: runOptions.fileInfo.name,
+      defaultEscapeCharacter: runOptions.settings.commonStyles.escapeCharacter,
+    });
 
     [newText] = BlockquoteStyle.applyIfEnabled(newText, runOptions.settings, this.disabledRules);
 
