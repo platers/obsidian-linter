b04ec0d7a17bd38cb94fd1d755b24163e05772d5
sec-ml <150138296+sec-ml@users.noreply.github.com>
2026-02-24 16:04:58 +0000

    Update ja.ts
    Co-authored-by: Peter Kaufman <peterjameskaufman@gmail.com>

--- src/lang/locale/ja.ts
+++ src/lang/locale/ja.ts
@@ -193,7 +193,7 @@ export default {
       },
       'additional-file-extensions': {
         'name': '追加のファイル拡張子',
-        'description': 'md に加えて Lint するファイル拡張子。例: mdx、svx。先頭のドットは含めません。注: 追加した拡張子に関係なく、Obsidian が Markdown として認識するファイル（ネイティブまたは他のプラグイン経由）のみが Lint されます。',
+        'description': 'md に加えて Lint するファイル拡張子。例: mdx、svx。先頭のドットは含めません。<b>注: 追加した拡張子に関係なく、Obsidian が Markdown として認識するファイル（ネイティブまたは他のプラグイン経由）のみが Lint されます。</b>',
         'extension-placeholder': '例: mdx',
         'add-input-button-text': '拡張子を追加',
         'delete-tooltip': '削除',
