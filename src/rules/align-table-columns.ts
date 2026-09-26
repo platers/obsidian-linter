dfa1fae4fcd3344bae728bd7977f8dd0b2ee683b
Peter Kaufman <PeterJamesKaufman@gmail.com>
2026-09-26 12:26:07 -0400

    include cleaned up code and new differential

--- src/rules/align-table-columns.ts
+++ src/rules/align-table-columns.ts
@@ -36,23 +36,7 @@ export default class AlignTable extends RuleBuilder<AlignTableOptions> {
     let projectedText = projection.text;
     for (const tablePosition of tablePositions) {
       const tableText = projectedText.substring(tablePosition.startIndex, tablePosition.endIndex);
-      const startIndex = getStartOfLineIndex(projectedText, tablePosition.startIndex);
-
-      let startOfLine: string;
-      if (startIndex === tablePosition.startIndex) { // make sure we didn't accidentally already include the whitespace at the start of the table
-        let endOfStartOfLine = startIndex;
-        while (endOfStartOfLine < tablePosition.endIndex) {
-          const char = projectedText.charAt(endOfStartOfLine);
-          if (char.trim() !== '') {
-            break;
-          }
-
-          endOfStartOfLine++;
-        }
-        startOfLine = projectedText.substring(startIndex, endOfStartOfLine);
-      } else {
-        startOfLine = projectedText.substring(startIndex, tablePosition.startIndex);
-      }
+      const startOfLine = projectedText.substring(getStartOfLineIndex(projectedText, tablePosition.startIndex), tablePosition.startIndex);
 
       fmt = new MarkdownTableFormatter();
       formatedTable = fmt.formatTable(tableText, startOfLine);
